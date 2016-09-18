278f2d13e78cf09a0f360af8c610bdbb9afb146d
Ong Mur Fei <ongmurfei@gmail.com>
2016-09-18 12:44:47 +0100

    testpush

--- dataExtract.py
+++ dataExtract.py
@@ -1,12 +1,14 @@
 # Data Extraction [Inference Module]
 # Currently supports a few instances of quandl imports
 # Attempts to infer call to closest match (Edit Distance)
+import yahoo_finance as yahoo
 import quandl
 import distance
 
 # List of functions to extract data
 func = {
     "quandl": lambda x: quandl.get(x[0]),
+    "yahoo": lambda x: yahoo.Share(x[0]).get_historical('2016-05-05', '2016-06-06')
 }
 
 # Collection of extraction methods and arguments
@@ -15,6 +17,7 @@ Collection = {
     # corresponding to [(function), [argument list]]
     "US Rates": [func["quandl"], ["FED/SVENY"]],
     "USD/SGD FX": [func["quandl"], ["FED/RXI_N_A_SI"]],
+    "Google": [func["yahoo"], ["GOOGL"]]
 }
 
 
--- quanl_test.py
+++ quanl_test.py
@@ -2,9 +2,14 @@ import matplotlib.pyplot as plt
 from dataExtract import *
 
 # FED
-fx_usd_sgd = extract("usd sgd fx")
+fx_usd_sgd = extract("usd/sgd fx")
 zero_Rates = extract("US zero rates")
+google_px = extract("google")
 
-plt.plot(fx_usd_sgd)
+# plt.plot(fx_usd_sgd)
 
+
+print zero_Rates
+print google_px
 print fx_usd_sgd
+
